6f6703165972370aa12460b8730ebace700bda61
DESKTOP-R11DFAK\Tohsib Qureshi <tohsib.qureshi.57@gmail.com>
2020-10-07 21:56:35 +0530

    first commit

---
+++ CPlacementQuestions/app/src/main/java/com/outsideandroider/cplacementquestions/MainActivity.java
@@ -0,0 +1,25 @@
+package com.outsideandroider.cplacementquestions;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this,QuestionsActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
